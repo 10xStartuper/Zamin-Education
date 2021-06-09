164ba1332c50645d362ae63a92dee1ab1b29f69d
Javohir Nurmatjonov <javoxirone@gmail.com>
2021-06-09 19:21:42 +0500

    Added new pages and changed course-detail desgin

--- js/script.js
+++ js/script.js
@@ -53,3 +53,18 @@ $(document).ready(function(){
         $( '.header__menu-adaptive' ).toggleClass( "active" );
       });
 })
+
+
+function openCity(evt, courseName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(courseName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
