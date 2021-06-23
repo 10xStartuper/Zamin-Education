91cad0c819c471c9484b70c96e551b2a2f51bdc1
Mukhammadyusuf Abdurakhimov <icoderx@yandex.ru>
2021-06-23 22:37:07 +0500

    Some minor updates

--- js/script.js
+++ js/script.js
@@ -1,59 +1,78 @@
-$(document).ready(function(){
-    $('.slider-for').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: true,
-        fade: true,
-        asNavFor: '.slider-nav'
-      });
-      $('.slider-nav').slick({
-        slidesToShow: 5,
-        slidesToScroll: 1,
-        asNavFor: '.slider-for',
-        focusOnSelect: true,
-        arrows: false,
-      });
+$(document).ready(function () {
+  $(".slider-for").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    asNavFor: ".slider-nav",
+  });
+  $(".slider-nav").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: ".slider-for",
+    focusOnSelect: true,
+    arrows: false,
+  });
 
-      $('.partner__slider').slick({
-        slidesToShow: 3,
-        centerMode: true,
-        autoplay: true,
-        arrows: true,
-        dots: true,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
-        ]
-      })
-      $( '.menu-burger' ).click(function() {
-        $( '.header__menu-adaptive' ).toggleClass( "active" );
-      });
-})
+  $(".partner__slider").slick({
+    slidesToShow: 3,
+    centerMode: true,
+    autoplay: true,
+    arrows: true,
+    dots: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+  $(".menu-burger").click(function () {
+    $(".header__menu-adaptive").toggleClass("active");
+  });
+  var $videoSrc;
+  $(".video-btn").click(function () {
+    $videoSrc = $(this).data("src");
+  });
+  console.log($videoSrc);
 
+  // when the modal is opened autoplay it
+  $("#myModal").on("shown.bs.modal", function (e) {
+    // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+    $("#video").attr(
+      "src",
+      $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+    );
+  });
+
+  // stop playing the youtube video when I close the modal
+  $("#myModal").on("hide.bs.modal", function (e) {
+    // a poor man's stop video
+    $("#video").attr("src", $videoSrc);
+  });
+});
 
 function openCity(evt, courseName) {
   let i, tabcontent, tablinks;
@@ -69,28 +88,25 @@ function openCity(evt, courseName) {
   evt.currentTarget.className += " active";
 }
 
-
 let acc = document.getElementsByClassName("accordion");
 let i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function () {
     this.classList.toggle("active");
     let panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
     } else {
       panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
+    }
   });
 }
 
-
-
-let burger = document.querySelector('.fa-bars')
-let dashboard = document.querySelector('.lesson__dashboard')
-let lessonPlay = document.querySelector('.lesson__play')
-burger.addEventListener('click', () => {
-  dashboard.classList.toggle('active')
-  lessonPlay.classList.toggle('active')
-})
+let burger = document.querySelector(".fa-bars");
+let dashboard = document.querySelector(".lesson__dashboard");
+let lessonPlay = document.querySelector(".lesson__play");
+burger.addEventListener("click", () => {
+  dashboard.classList.toggle("active");
+  lessonPlay.classList.toggle("active");
+});
